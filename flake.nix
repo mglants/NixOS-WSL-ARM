c44019f35ab370fa00c31b5d7f096cd64f0c27e5
Joshua Peek <josh@users.noreply.github.com>
2024-08-17 02:49:25 +0000

    Add initial NixOS-WSL builder flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    nixos-wsl = {
+      url = "github:nix-community/NixOS-WSL?ref=main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-wsl }: {
+    packages.aarch64-linux.default =
+      self.nixosConfigurations.nixos.config.system.build.tarballBuilder;
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      modules = [
+        nixos-wsl.nixosModules.default
+        {
+          wsl.enable = true;
+          system.stateVersion = "24.11";
+        }
+      ];
+    };
+  };
+}

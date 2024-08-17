ec4da9e7f8f485bf1abe6b6ceae9b854c0d60404
Joshua Peek <josh@users.noreply.github.com>
2024-08-17 04:41:37 +0000

    nixfmt

--- flake.nix
+++ flake.nix
@@ -7,38 +7,43 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixos-wsl }: {
-    packages.aarch64-linux.default =
-      self.nixosConfigurations.nixos.config.system.build.tarballBuilder;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-wsl,
+    }:
+    {
+      packages.aarch64-linux.default = self.nixosConfigurations.nixos.config.system.build.tarballBuilder;
 
-    packages.x86_64-linux.default =
-      self.nixosConfigurations.nixos-x86_64.config.system.build.tarballBuilder;
+      packages.x86_64-linux.default =
+        self.nixosConfigurations.nixos-x86_64.config.system.build.tarballBuilder;
 
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        nixos-wsl.nixosModules.default
-        {
-          wsl.enable = true;
-          system.stateVersion = "24.11";
-        }
-      ];
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          nixos-wsl.nixosModules.default
+          {
+            wsl.enable = true;
+            system.stateVersion = "24.11";
+          }
+        ];
+      };
+
+      nixosConfigurations.nixos-x86_64 = nixpkgs.lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+          nixos-wsl.nixosModules.default
+          {
+            nixpkgs.config.allowUnsupportedSystem = true;
+            nixpkgs.hostPlatform.system = "aarch64-linux";
+            nixpkgs.buildPlatform.system = "x86_64-linux";
+          }
+          {
+            wsl.enable = true;
+            system.stateVersion = "24.11";
+          }
+        ];
+      };
     };
-
-    nixosConfigurations.nixos-x86_64 = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-      modules = [
-        nixos-wsl.nixosModules.default
-        {
-          nixpkgs.config.allowUnsupportedSystem = true;
-          nixpkgs.hostPlatform.system = "aarch64-linux";
-          nixpkgs.buildPlatform.system = "x86_64-linux";
-        }
-        {
-          wsl.enable = true;
-          system.stateVersion = "24.11";
-        }
-      ];
-    };
-  };
 }

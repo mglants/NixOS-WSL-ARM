1400ae04e894f5761c058acb957ee3c544b88372
Joshua Peek <josh@users.noreply.github.com>
2024-08-17 07:24:50 +0000

    Cross build is just too slow

--- flake.nix
+++ flake.nix
@@ -16,28 +16,10 @@
     {
       packages.aarch64-linux.default = self.nixosConfigurations.nixos.config.system.build.tarballBuilder;
 
-      # packages.x86_64-linux.default =
-      #   self.nixosConfigurations.nixos-x86_64.config.system.build.tarballBuilder;
-
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
         modules = [
           nixos-wsl.nixosModules.default
-          {
-            wsl.enable = true;
-            system.stateVersion = "24.11";
-          }
-        ];
-      };
-
-      nixosConfigurations.nixos-x86_64 = nixpkgs.lib.nixosSystem {
-        modules = [
-          nixos-wsl.nixosModules.default
-          {
-            nixpkgs.config.allowUnsupportedSystem = true;
-            nixpkgs.hostPlatform.system = "aarch64-linux";
-            nixpkgs.buildPlatform.system = "x86_64-linux";
-          }
           {
             wsl.enable = true;
             system.stateVersion = "24.11";

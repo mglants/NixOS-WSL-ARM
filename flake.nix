c62a74dbecc8373808f1e5f39169ba376d8ab021
Joshua Peek <josh@users.noreply.github.com>
2024-08-17 07:50:35 +0000

    Enable flakes out of the box

--- flake.nix
+++ flake.nix
@@ -24,6 +24,12 @@
             wsl.enable = true;
             system.stateVersion = "24.11";
           }
+          {
+            nix.settings.experimental-features = [
+              "nix-command"
+              "flakes"
+            ];
+          }
         ];
       };
     };
